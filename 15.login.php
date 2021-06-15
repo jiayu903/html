b2f7c82a438e6e3a328d1c03f450aff8a18dc563
jiayu903 <82023328+jiayu903@users.noreply.github.com>
2021-06-15 16:39:43 +0800

    Add files via upload

---
+++ 15.login.php
@@ -0,0 +1,8 @@
+<?php
+     if($_POST["id"]=="tony" && $_POST["pwd"]=="1234")
+        echo "login success";             
+     else
+        echo "login fail";
+?>
+
+<!--echo 的功用是用來輸出字串，是使用率非常高的一個語言結構。-->
